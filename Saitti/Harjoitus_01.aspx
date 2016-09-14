780480229ea4786f124825d3b86cb797cedea418
JanKuukkanen <jankuukkanen@gmail.com>
2016-09-15 02:31:52 +0300

    Tehtävä 1 tehty
    Laskuista saatavat arvot eivät täysin vastaa ohjeessa olevaa esimerkkiä

---
+++ Saitti/Harjoitus_01.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Harjoitus_01.aspx.cs" Inherits="Saitti_Harjoitus_01" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title>Ikkunan tarjouslaskuri</title>
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="calculator">
+        <div id="inputs" class="w3-green" style="height:120px;">
+            <asp:Label ID="lblwidth" runat="server" Style="position:fixed; left:5px; top:0px;">Leveys(mm): </asp:Label> <asp:TextBox ID="txtwidth" runat="server" Style="position:fixed; left:190px; top:0px;"></asp:TextBox><br />
+            <asp:Label ID="lblheight" runat="server" Style="position:fixed; left:5px; top:27px;">korkeus(mm): </asp:Label> <asp:TextBox ID="txtheight" runat="server" Style="position:fixed; left:190px; top:27px;"></asp:TextBox><br />
+            <asp:Label ID="lblframe" runat="server" Style="position:fixed; left:5px; top:55px;">Karmipuun leveys(mm): </asp:Label> <asp:TextBox ID="txtframe" runat="server" Style="position:fixed; left:190px; top:55px; width:35px">45</asp:TextBox>
+            <asp:Button ID="btncalculatePrice" runat="server" Text="Laske Tarjoushinta" Style="position:fixed; top:85px; left: 5px; width:150px" OnClick="btncalculatePrice_Click" />
+        </div>
+        <p>---------------------------------------------------------</p>
+        <div id="results" class="w3-red" style="height:80px;">
+            <asp:Label ID="lblwindowArea" runat="server" Style="margin-left:5px;">Ikkunan pinta-ala: </asp:Label> <asp:Label ID="lblwindowAreaResult" runat="server" Style="margin-left:56px">...</asp:Label><br />
+            <asp:Label ID="lblframeRad" runat="server" Style="margin-left:5px;">Karmin piiri: </asp:Label> <asp:Label ID="lblframeRadResult" runat="server" Style="margin-left:103px;">...</asp:Label><br />
+            <asp:Label ID="lblprice" runat="server" Style="margin-left:5px;">Tarjoushinta(Ilman ALV): </asp:Label> <asp:Label ID="lblpriceResult" runat="server" Style="margin-left:4px;">...</asp:Label>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/Harjoitus_01.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Saitti_Harjoitus_01 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void btncalculatePrice_Click(object sender, EventArgs e)
+    {
+        //Hinta (ilman alvia) = (1 + kate%) x ((IkkunanPintaAla x LasinNeliohinta) + (KarminPiiri x AlumiiniKarminJuoksumetriHinta) + (Työmenekki))
+        //Käytetään tehtävässä seuraavia parametrejä:
+        //kate% = 30%  LasinNelioHinta=45€/m2  AlumiiniKarminJuoksumetriHinta=100€/jm  Työmenekki=150€/ikkuna
+
+        int checkfordigits = 0;
+
+        if (!txtwidth.Text.Equals("") && !txtheight.Text.Equals("") && !txtframe.Text.Equals(""))
+        {
+            checkfordigits = checkDigit(txtwidth.Text, txtheight.Text, txtframe.Text);
+        }
+
+        if (checkfordigits == 4)
+        {
+            double leveys = Convert.ToInt32(txtwidth.Text); // ikkunan leveys
+            double korkeus = Convert.ToInt32(txtheight.Text); // ikkunan korkeus
+            double karmi = Convert.ToInt32(txtframe.Text); // karmipuun leveys
+
+            double ikkunanpintaala = leveys * korkeus; // ikkunan pinta-ala
+
+            double kokonaispintaala = (korkeus + (karmi * 2)) * (leveys + (karmi * 2)); // pinta-ala karmin kanssa
+            double karminpintaala = kokonaispintaala - ikkunanpintaala;
+
+            double karminpiiri = ((leveys + karmi * 2) * 2) + ((korkeus + karmi * 2) * 2);
+            double hinta = ((1.0 + 0.3) * ((ikkunanpintaala / 1000000.0) * 45.0) + ((karminpiiri / 1000.0) * 100.0) + (150.0));
+
+            lblwindowAreaResult.Text = (ikkunanpintaala / 1000000).ToString() + " m2";
+            lblframeRadResult.Text = (karminpiiri / 1000).ToString() + " m";
+
+            lblpriceResult.Text = (hinta).ToString() + " €";
+        }
+    }
+
+    public int checkDigit(string width, string height, string frame)
+    {
+        int i = 0;
+        int notdigit = 4;
+
+        while (i < width.Count())
+        {
+            if (!char.IsDigit(width[i]))
+            {
+                notdigit = 1;
+                txtwidth.Text = "Syötä ainoastaan numeroita";
+            }
+            i = i + 1;
+        }
+
+        i = 0;
+
+        while (i < height.Count())
+        {
+            if (!char.IsDigit(height[i]))
+            {
+                notdigit = 2;
+                txtheight.Text = "Syötä ainoastaan numeroita";
+            }
+            i = i + 1;
+        }
+
+        i = 0;
+
+        while (i < frame.Count())
+        {
+            if (!char.IsDigit(frame[i]))
+            {
+                notdigit = 3;
+                txtframe.Text = "Syötä ainoastaan numeroita";
+            }
+            i = i + 1;
+        }
+        return notdigit;
+    }
+}
