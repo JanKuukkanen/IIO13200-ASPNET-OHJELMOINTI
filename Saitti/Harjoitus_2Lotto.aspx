889dbb00e2f59e944df45fb54e5f9d19dbe75981
JanKuukkanen <jankuukkanen@gmail.com>
2016-09-22 15:20:28 +0300

    Tehtävä 2 kesken
    BLLotto luokka ei palauta numeroita

---
+++ Saitti/Harjoitus_2Lotto.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Harjoitus_2Lotto.aspx.cs" Inherits="Saitti_Harjoitus_2Lotto" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Lotto 2016</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lblLottery" runat="server" Text="Choose lottery type:" Style="vertical-align:middle;"></asp:Label>
+        <asp:DropDownList ID="ddlLottery" runat="server" Style="vertical-align:middle; margin-left:20px;" OnSelectedIndexChanged="Page_Load"></asp:DropDownList><br />
+        <asp:Label ID="lblDraws" runat="server" Text="Number of draws:" Style="vertical-align:middle; padding-top:20px;"></asp:Label>
+        <asp:TextBox ID="txtDraws" runat="server" Style="vertical-align:middle; margin-left:20px;"></asp:TextBox><br />
+        <asp:Button ID="btnDraws" runat="server" Text="Draw" OnClick="btnDraws_Click" />
+        <asp:Button ID="Clear" runat="server" Text="Clear" Style="margin-left:10px;" OnClick="Clear_Click"/><br /><br />
+        <asp:Label ID="lblNumbers" runat="server" Text="Randomly drawn numbers: "></asp:Label><br />
+        <asp:ListBox ID="lsbNumbers" runat="server"></asp:ListBox><br />
+    </div>
+    <div style="margin-top:20px;">
+        <asp:Label ID="lblAlert" runat="server" Text="..."></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Saitti/Harjoitus_2Lotto.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using JAMK.ICT.BL;
+
+public partial class Saitti_Harjoitus_2Lotto : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (ddlLottery.Items.FindByText("Suomi") == null)
+        {
+            ddlLottery.Items.Add("Suomi");
+            ddlLottery.Items.Add("VikingLotto");
+            ddlLottery.SelectedValue = "Suomi";
+        }
+    }
+
+    protected void btnDraws_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            List<int> results = new List<int>();
+
+            Regex pattern = new Regex("^[1-9]+$");
+            if (!pattern.IsMatch(txtDraws.Text))
+            {
+                lblAlert.Text = "Please specify a number higher than 0 for the lottery";
+            }
+            else if (txtDraws.Text == "")
+            {
+                lblAlert.Text = "Please specify a number higher than 0 for the lottery";
+            }
+            else
+            {
+                int drawnumber = int.Parse(txtDraws.Text);
+
+                for (int i = 0; i != drawnumber; i++)
+                {
+                    if (i >= 1 || lsbNumbers.Items.Count == 0)
+                    {
+                        lsbNumbers.Items.Add("Next lottery");
+                    }
+
+                    Lotto lotto = new Lotto();
+
+                    results = lotto.ArvoRivi();
+
+                    lblAlert.Text = results.Count.ToString();
+
+                    for (int nro = 0; nro < results.Count; nro++)
+                    {
+                        lsbNumbers.Items.Add(results[nro].ToString());
+                    }
+                }
+            }
+        }
+        catch (Exception er)
+        {
+            lblAlert.Text = "Invalid input: " + er;
+        }
+    }
+
+    protected void Clear_Click(object sender, EventArgs e)
+    {
+        lsbNumbers.Items.Clear();
+    }
+}
